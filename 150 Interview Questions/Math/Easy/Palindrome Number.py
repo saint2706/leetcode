5efcce22138f641336eaf2ff2b2556e0778ce76b
Saint <arishabh748@gmail.com>
2025-10-17 19:51:06 +0530

    Use arithmetic palindrome check

--- 150 Interview Questions/Math/Easy/Palindrome Number.py
+++ 150 Interview Questions/Math/Easy/Palindrome Number.py
@@ -1,6 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x1 = str(x)
-        x2 = str(x)[::-1]
-        # print(x1 == x2)
-        return x1 == x2
+        if x < 0:
+            return False
+        if x % 10 == 0 and x != 0:
+            return False
+
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+
+        return x == reversed_half or x == reversed_half // 10
